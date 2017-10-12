06afad54ff1e20a4495b57b03175ba2d25f580f2
Brian McVeigh <bcmcveigh@gmail.com>
2017-10-11 21:03:08 -0400

    Do not rely on composer to get singleton package.

---
+++ src/Singleton/SingletonInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\singleton;
+
+/**
+ * Represents the required functions for a Singleton.
+ */
+interface SingletonInterface {
+
+  /**
+   * Returns instance, if instance does not exist then creates new one and returns it
+   *
+   * @return $this
+   */
+  public static function getInstance();
+
+}
